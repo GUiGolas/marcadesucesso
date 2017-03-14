bd09a95c7b9f254f00eb419a85a7eb5d49e4e599
GUiGolas <gui2519@hotmail.com>
2017-03-14 01:54:44 -0300

    - Acertado a borda do form na página inicial - Retirado os placeholders do form que estavam redundantes e em inglês - Acrescentado o script do google analytics na página inicial

--- index.php
+++ index.php
@@ -42,7 +42,22 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-</head>
+
+		<!-- Google Analytics -->
+
+		<script>
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			  ga('create', 'UA-93681806-1', 'auto');
+			  ga('send', 'pageview');
+
+		</script>
+		<!-- End Google Analytics -->
+
+	</head>
 
 <body>
 
@@ -117,9 +132,11 @@
     border-radius: 15px;
     height: 394px;
     box-shadow: -3px 6px 11px rgba(0, 0, 0, 0.18);
-    margin-bottom: 240px;"   
+    margin-bottom: 70px;
+	height:57%;
+	"   
     	
-    frameborder="0"></iframe> 
+    frameborder="0" scrolling="no"></iframe> 
 
 </div>
 	
